40a22e331323b8238013fd2dab7db228e3e9e459
ilhanB <ilhans70@gmail.com>
2025-07-03 12:47:25 +0200

    kod ec2 için güncellendi

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def head():
-     return 'Hello world Oktay'
+     return 'Hello world Ilhan'
 
 @app.route('/secondpage')
 def second():
@@ -18,5 +18,5 @@ def fourth(id):
 
 if __name__ == '__main__':
 
-     app.run(debug=True)
-     # app.run(host= '0.0.0.0', port=8080)
+     #app.run(debug=True)
+     app.run(host= '0.0.0.0', port=80)
