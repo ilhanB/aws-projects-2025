536cbdd67a011191e7a2344ca40813c168157d42
ilhanB <ilhans70@gmail.com>
2025-08-20 14:16:18 +0200

    name updated

--- 001-roman-numerals-converter/roman-numerals-converter-app.py
+++ 001-roman-numerals-converter/roman-numerals-converter-app.py
@@ -23,7 +23,7 @@ def main_post():
             return render_template('index.html', developer_name='ilhan', not_valid=True)
         return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='ilhan')
     else:
-        return render_template('index.html', developer_name='oktay', not_valid=False)
+        return render_template('index.html', developer_name='ilhan', not_valid=False)
 
 if __name__ == '__main__':
     # app.run(debug=True)
